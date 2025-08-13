25aa205d2ae28ca68f9e2e93bb4fbc292819a2d0
Jon-natur <jon@natur360.dk>
2025-08-13 11:54:49 +0200

    Add files via upload

---
+++ layers/EjbyMose_besigtigelsesarealer_2022_0.js
@@ -0,0 +1,1 @@
+var json_EjbyMose_besigtigelsesarealer_2022_0 = {"type":"FeatureCollection","name":"EjbyMose_besigtigelsesarealer_2022_0","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
